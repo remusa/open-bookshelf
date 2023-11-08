ed535c414fb4ab3b8ab8add04e315a89b3e19ba9
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-11-07 21:31:14 -0600

    chore: remove prerender from pages

--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -4,10 +4,6 @@ import type { PageLoad } from './$types'
 
 export const csr = dev
 
-export const prerender = true
-
-export const ssr = false
-
 export const load: PageLoad = async () => {
 	// const stores = await import('$lib/utils/stores')
 	// const settings = (await stores.settingsStore.get('settings')) as SettingsType
