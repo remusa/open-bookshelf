8dfaa13d50942f05731e19a4f0655fee6daea3a2
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-11-07 21:30:08 -0600

    chore: use trailing slash always for spa

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -17,13 +17,21 @@ for (const path in modules) {
 	})
 }
 
-export const load: LayoutLoad = async ({}) => {
+export const load: LayoutLoad = async () => {
 	const menu = await Promise.all(body.filter((item) => item.title))
 	return {
 		menu,
 	}
 }
 
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
 export const prerender = true
 
 export const ssr = false
+
+export const trailingSlash = 'always'
+
+// we don't need any JS on this page, though we'll load
+// it in dev so that we get hot module replacement
+// export const csr = dev
